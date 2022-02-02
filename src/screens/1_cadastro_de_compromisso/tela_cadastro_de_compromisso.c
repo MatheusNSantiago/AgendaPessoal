eb401606b1253e6f4778d5ded30c92234c30a87b
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2022-02-02 10:02:38 -0300

    Tirar os valores hardcoded

--- src/screens/1_cadastro_de_compromisso/tela_cadastro_de_compromisso.c
+++ src/screens/1_cadastro_de_compromisso/tela_cadastro_de_compromisso.c
@@ -3,7 +3,6 @@
 #include <string.h>
 #include <stdio.h>
 
-
 void tela_cadastro_de_compromisso(NoCompromisso **l)
 {
     Data data;
@@ -11,42 +10,24 @@ void tela_cadastro_de_compromisso(NoCompromisso **l)
     Endereco local;
     char descricao[500], observacao[150];
 
-    // printf("|-----------------| Cadastrar compromisso |-----------------|\n");
-    // printf("(1/6) Data\n");
-    // data = nova_data();
-    data.dia = 1;
-    data.mes = 1;
-    data.ano = 2022;
+    printf("|-----------------| Cadastrar compromisso |-----------------|\n");
+    printf("(1/6) Data\n");
+    data = nova_data();
 
-    // printf("(2/6) Começa as\n");
+    printf("(2/6) Começa as\n");
     h_ini = novo_horario();
-    // h_ini.hora = 1;
-    // h_ini.minuto = 1;
 
-    // printf("(3/6) Termina as\n");
-    // h_fim = novo_horario();
-    h_fim.hora = 13;
-    h_fim.minuto = 30;
+    printf("(3/6) Termina as\n");
+    h_fim = novo_horario();
 
-    // printf("(4/6) Local\n");
-    // local = novo_endereco();
-    strcpy(local.bairro, "Bairro");
-    strcpy(local.cep, "CEP");
-    strcpy(local.cidade, "CIDADE");
-    strcpy(local.complemento, "COMPLEMENTO");
-    strcpy(local.estado, "ESTADO");
-    strcpy(local.nome_logradouro, "Nome Logradouro");
-    strcpy(local.tipo_logradouro, "Tipo Logradouro");
-    strcpy(local.pais, "BRASIL");
-    local.numero = 12;
+    printf("(4/6) Local\n");
+    local = novo_endereco();
 
-    // printf("(5/6) Descrição: ");
-    // ler_string(descricao, 500);
-    strcpy(descricao, "DESCRIÇÃO");
+    printf("(5/6) Descrição: ");
+    ler_string(descricao, 500);
 
-    // printf("\n(6/6) Observação: ");
-    // ler_string(observacao, 150);
-    strcpy(observacao, "OBSERVACAO");
+    printf("\n(6/6) Observação: ");
+    ler_string(observacao, 150);
 
     Compromisso c = cria_compromisso(data, h_ini, h_fim, local, descricao, observacao);
 
@@ -59,14 +40,12 @@ void tela_cadastro_de_compromisso(NoCompromisso **l)
             return;
         }
 
-    insere_compromisso(l, c); // TODO Tirar isso dps
+    if (escolher("Confirmar Inclusão"))
+    {
+        insere_compromisso(l, c);
+        puts("\nCompromisso inserido com sucesso!\n");
+    }
 
-    // if (escolher("Confirmar Inclusão"))
-    // {
-    //     insere_compromisso(l, c);
-    //     puts("\nCompromisso inserido com sucesso!\n");
-    // }
-
-    // if (escolher("Inserir outro compromisso?"))
-    //     tela_cadastro_de_compromisso(l);
+    if (escolher("Inserir outro compromisso?"))
+        tela_cadastro_de_compromisso(l);
 }
--- src/screens/2_cadastro_de_contato/tela_cadastro_de_contato.c
+++ src/screens/2_cadastro_de_contato/tela_cadastro_de_contato.c
@@ -10,62 +10,39 @@ void tela_cadastro_de_contato(NoContato **l)
     Contato c;
     while (true)
     {
-        // do
-        // {
-        //     puts("(1/6) Nome");
-        //     ler_string(c.nome, 50);
+        do
+        {
+            puts("(1/6) Nome");
+            ler_string(c.nome, 50);
 
-        //     if (strlen(c.nome) == 0)
-        //         puts("Nome não pode ficar em branco");
+            if (strlen(c.nome) == 0)
+                puts("Nome não pode ficar em branco");
 
-        // } while (strlen(c.nome) == 0);
+        } while (strlen(c.nome) == 0);
 
-        // puts("(2/6) Local de Trabalho");
-        // ler_string(c.local_de_trabalho, 100);
+        puts("(2/6) Local de Trabalho");
+        ler_string(c.local_de_trabalho, 100);
 
-        // puts("(3/6) Telefones para contato");
-        // c.telefones[0] = novo_telefone();
-        // if (escolher("Adicionar outro telefone?"))
-        //     c.telefones[1] = novo_telefone();
+        puts("(3/6) Telefones para contato");
+        c.telefones[0] = novo_telefone();
+        if (escolher("Adicionar outro telefone?"))
+            c.telefones[1] = novo_telefone();
 
-        // puts("(4/6) Endereço completo");
-        // c.endereco_completo = novo_endereco();
+        puts("(4/6) Endereço completo");
+        c.endereco_completo = novo_endereco();
 
-        // puts("(5/6) Data de nascimento");
-        // c.data_de_nascimento = nova_data();
+        puts("(5/6) Data de nascimento");
+        c.data_de_nascimento = nova_data();
 
-        // puts("(6/6) Observação");
-        // ler_string(c.observacao, 500);
-
-        c.data_de_nascimento.ano = 1999;
-        c.data_de_nascimento.mes = 7;
-        c.data_de_nascimento.dia = 19;
-
-        strcpy(c.endereco_completo.bairro, "Bairro");
-        strcpy(c.endereco_completo.cep, "CEP");
-        strcpy(c.endereco_completo.cidade, "CIDADE");
-        strcpy(c.endereco_completo.complemento, "COMPLEMENTO");
-        strcpy(c.endereco_completo.estado, "ESTADO");
-        strcpy(c.endereco_completo.nome_logradouro, "Nome Logradouro");
-        strcpy(c.endereco_completo.tipo_logradouro, "Tipo Logradouro");
-        strcpy(c.endereco_completo.pais, "BRASIL");
-        c.endereco_completo.numero = 12;
-
-        strcpy(c.local_de_trabalho, "Perto do lado do outro lado");
-        ler_string(c.nome, 50);
-        strcpy(c.observacao, "OBS");
-        strcpy(c.telefones[0].codigo_pais, "55");
-        strcpy(c.telefones[0].ddd, "21");
-        strcpy(c.telefones[0].numero, "993823598");
-
-        inserir_contato_em_ordem_alfabetica(l, c);
-        //|-----------------------------------------||-----------------------------------------|
-        // if (escolher("Confirmar inclusão?"))
-        // {
-        //     inserir_contato_em_ordem_alfabetica(l, c);
-        //     puts("\nContato foi inserido com sucesso!\n");
-        //     dormir(1);
-        // }
+        puts("(6/6) Observação");
+        ler_string(c.observacao, 500);
+        
+        if (escolher("Confirmar inclusão?"))
+        {
+            inserir_contato_em_ordem_alfabetica(l, c);
+            puts("\nContato foi inserido com sucesso!\n");
+            dormir(1);
+        }
 
         if (escolher("Inserir outro?") == false)
             return;
